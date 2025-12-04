25ab31f5c726005f5b7de70cf72474f2da57db2a
Dennis Barbosa <dennisbarbosa998@gmail.com>
2025-12-03 21:00:07 -0300

    feat: enviando questões da Aula03 Douglas

---
+++ ProfessorDouglas/Aula03/exemple.js
@@ -0,0 +1,31 @@
+// const kwh = 100
+
+// if (kwh <= 100) {
+//     console.log('O valor da sua tarifa é R$ 0,50 por kwh')
+// }else if(kwh <= 200){
+//     console.log('O valor da sua tarifa é R$ 0,75 por kwh')
+// }else {
+//     console.log('O valor da sua tarifa é R$ 1,00 por kwh')
+// }
+
+// let impostoDeRenda = 0
+// let salarioMensal = 4990
+
+// if (salarioMensal <= 2000) {
+//   console.log('isento')
+//   impostoDeRenda = salarioMensal * 0
+// } else if (salarioMensal <= 3500) {
+//   impostoDeRenda = salarioMensal * 0.10  
+// } else if (salarioMensal <= 5000) {
+//   impostoDeRenda = salarioMensal * 0.20  
+// } else {
+//   impostoDeRenda = salarioMensal * 0.30  
+// }
+
+// console.log(`O total do seu imposto é ${impostoDeRenda}`)
+
+let number = 16.64
+
+let result = number % 2 ? 'impar' : 'par'
+
+console.log(result)
